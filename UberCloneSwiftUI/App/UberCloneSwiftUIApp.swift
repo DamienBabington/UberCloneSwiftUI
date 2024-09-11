5610ba40feef0a851ce282a94f8764ec490d2192
DamienBabington <damien.babington@gmail.com>
2024-09-11 20:25:32 +0900

    Initial Commit

---
+++ UberCloneSwiftUI/App/UberCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneSwiftUIApp.swift
+//  UberCloneSwiftUI
+//
+//  Created by Damien Babington on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ UberCloneSwiftUI/Core/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  UberCloneSwiftUI
+//
+//  Created by Damien Babington on 9/11/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
