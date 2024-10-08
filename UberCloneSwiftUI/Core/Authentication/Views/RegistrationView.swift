8178cb02c84239586b0cd4e3cf760ba2aaee8554
DamienBabington <damien.babington@gmail.com>
2024-10-08 11:49:06 +0900

    Create RegistrationView

--- UberCloneSwiftUI/Core/Authentication/Views/RegistrationView.swift
+++ UberCloneSwiftUI/Core/Authentication/Views/RegistrationView.swift
@@ -2,126 +2,79 @@
 //  RegistrationView.swift
 //  UberCloneSwiftUI
 //
-//  Created by Damien Babington on 10/5/24.
+//  Created by Damien Babington on 10/8/24.
 //
 
 import SwiftUI
 
 struct RegistrationView: View {
-    @State var email = ""
-    @State var password = ""
+    @State private var fullName = ""
+    @State private var email = ""
+    @State private var password = ""
     
     var body: some View {
         ZStack {
-            Color.black
-                .ignoresSafeArea()
+            Color.black.ignoresSafeArea()
             
-            VStack() {
-                // image and title
-                VStack(spacing: -16) {
-                    Image("uber-app-icon")
-                        .resizable()
-                        .frame(width: 200, height: 200)
-                    
-                    Text("UBER")
-                        .foregroundStyle(.white)
-                        .font(.largeTitle)
-                }
-                
-                // input fields
-                VStack(spacing: 32) {
-                    CustomInputField(text: $email, 
-                                     title: "Email address",
-                                     placeholder: "name@example.com")
-
-                    CustomInputField(text: $password, 
-                                     title: "Password",
-                                     placeholder: "Enter your password",
-                                     isSecureField: true)
-                }
-                .padding(.horizontal)
-                .padding(.top, 12)
-                
+            VStack(alignment: .leading, spacing: 20) {
+                // back button
                 Button {
                     
                 } label: {
-                    Text("Forgot Password?")
-                        .font(.system(size: 13, weight: .semibold))
-                        .foregroundStyle(.white)
-                        .padding(.vertical)
-                        .padding(.trailing, 28)
+                    Image(systemName: "arrow.left")
+                        .font(.title)
+                        .imageScale(.medium)
+                        .padding()
                 }
-                .frame(maxWidth: .infinity, alignment: .trailing)
                 
-                // social sign-in view
+                Text("Create new account")
+                    .font(.system(size: 40))
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.leading)
+                    .frame(width: 250)
+                
+                Spacer()
+                                
                 VStack {
-                    HStack(spacing: 24) {
-                        Rectangle()
-                            .frame(width: 76, height: 1)
-                            .foregroundStyle(.white)
-                            .opacity(0.5)
+                    // input fields
+                    VStack(spacing: 56) {
+                        CustomInputField(text: $fullName,
+                                         title: "Full Name",
+                                         placeholder: "Enter your name")
                         
-                        Text("Sign in with social")
-                            .foregroundStyle(.white)
-                            .fontWeight(.semibold)
+                        CustomInputField(text: $email,
+                                         title: "Email",
+                                         placeholder: "Enter your email")
                         
-                        Rectangle()
-                            .frame(width: 76, height: 1)
-                            .foregroundStyle(.white)
-                            .opacity(0.5)
+                        CustomInputField(text: $password,
+                                         title: "Password",
+                                         placeholder: "Enter your password")
                     }
+                    .padding(.leading)
                     
-                    HStack(spacing: 20) {
-                        Button {
+                    Spacer()
+                    
+                    // signup button
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Text("SIGN UP")
+                                .foregroundStyle(.black)
                             
-                        } label: {
-                            Image("facebook-sign-in-icon")
-                                .resizable()
-                                .frame(width: 44, height: 44)
+                            Image(systemName: "arrow.right")
+                                .foregroundStyle(.black)
                         }
-                        
-                        Button {
-                            
-                        } label: {
-                            Image("google-sign-in-icon")
-                                .resizable()
-                                .frame(width: 44, height: 44)
-                        }
-                    }
-                }
-                .padding(.vertical)
-                
-                // sign-in button
-                Button {
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                    }                    
+                    .background(Color(.white))
+                    .cornerRadius(10)
+                    .padding(.vertical)
                     
-                } label: {
-                    HStack {
-                        Text("SIGN IN")
-                            .foregroundStyle(.black)
-                        
-                        Image(systemName: "arrow.right")
-                            .foregroundStyle(.black)
-                    }
-                }
-                .frame(width: UIScreen.main.bounds.width - 32, height: 50)
-                .background(Color(.white))
-                .cornerRadius(10)
-                .padding(.vertical)
-
-                // signup button
-                Button {
-                    
-                } label: {
-                    HStack {
-                        Text("Don't have an account?")
-                            .font(.system(size: 14))
-                        
-                        Text("Sign Up")
-                            .font(.system(size: 14, weight: .semibold))
-                    }
-                    .foregroundStyle(.white)
+                    Spacer()
                 }
             }
+            .foregroundStyle(.white)
         }
     }
 }
