97517d6a572caf0c2fcd9923c88ccef06563b874
DamienBabington <damien.babington@gmail.com>
2024-10-08 12:06:09 +0900

    Add navigation between login and signup views with NavigationStack

--- UberCloneSwiftUI/Core/Authentication/Views/LoginView.swift
+++ UberCloneSwiftUI/Core/Authentication/Views/LoginView.swift
@@ -12,114 +12,117 @@ struct LoginView: View {
     @State var password = ""
     
     var body: some View {
-        ZStack {
-            Color.black
-                .ignoresSafeArea()
-            
-            VStack() {
-                // image and title
-                VStack(spacing: -16) {
-                    Image("uber-app-icon")
-                        .resizable()
-                        .frame(width: 200, height: 200)
-                    
-                    Text("UBER")
-                        .foregroundStyle(.white)
-                        .font(.largeTitle)
-                }
+        NavigationStack {
+            ZStack {
+                Color.black
+                    .ignoresSafeArea()
                 
-                // input fields
-                VStack(spacing: 32) {
-                    CustomInputField(text: $email, 
-                                     title: "Email address",
-                                     placeholder: "name@example.com")
-
-                    CustomInputField(text: $password, 
-                                     title: "Password",
-                                     placeholder: "Enter your password",
-                                     isSecureField: true)
-                }
-                .padding(.horizontal)
-                .padding(.top, 12)
-                
-                Button {
-                    
-                } label: {
-                    Text("Forgot Password?")
-                        .font(.system(size: 13, weight: .semibold))
-                        .foregroundStyle(.white)
-                        .padding(.vertical)
-                        .padding(.trailing, 28)
-                }
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                
-                // social sign-in view
-                VStack {
-                    HStack(spacing: 24) {
-                        Rectangle()
-                            .frame(width: 76, height: 1)
-                            .foregroundStyle(.white)
-                            .opacity(0.5)
+                VStack() {
+                    // image and title
+                    VStack(spacing: -16) {
+                        Image("uber-app-icon")
+                            .resizable()
+                            .frame(width: 200, height: 200)
                         
-                        Text("Sign in with social")
+                        Text("UBER")
                             .foregroundStyle(.white)
-                            .fontWeight(.semibold)
-                        
-                        Rectangle()
-                            .frame(width: 76, height: 1)
-                            .foregroundStyle(.white)
-                            .opacity(0.5)
+                            .font(.largeTitle)
                     }
                     
-                    HStack(spacing: 20) {
-                        Button {
+                    // input fields
+                    VStack(spacing: 32) {
+                        CustomInputField(text: $email, 
+                                         title: "Email address",
+                                         placeholder: "name@example.com")
+
+                        CustomInputField(text: $password, 
+                                         title: "Password",
+                                         placeholder: "Enter your password",
+                                         isSecureField: true)
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 12)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Forgot Password?")
+                            .font(.system(size: 13, weight: .semibold))
+                            .foregroundStyle(.white)
+                            .padding(.vertical)
+                            .padding(.trailing, 28)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    
+                    // social sign-in view
+                    VStack {
+                        HStack(spacing: 24) {
+                            Rectangle()
+                                .frame(width: 76, height: 1)
+                                .foregroundStyle(.white)
+                                .opacity(0.5)
                             
-                        } label: {
-                            Image("facebook-sign-in-icon")
-                                .resizable()
-                                .frame(width: 44, height: 44)
+                            Text("Sign in with social")
+                                .foregroundStyle(.white)
+                                .fontWeight(.semibold)
+                            
+                            Rectangle()
+                                .frame(width: 76, height: 1)
+                                .foregroundStyle(.white)
+                                .opacity(0.5)
                         }
                         
-                        Button {
+                        HStack(spacing: 20) {
+                            Button {
+                                
+                            } label: {
+                                Image("facebook-sign-in-icon")
+                                    .resizable()
+                                    .frame(width: 44, height: 44)
+                            }
                             
-                        } label: {
-                            Image("google-sign-in-icon")
-                                .resizable()
-                                .frame(width: 44, height: 44)
+                            Button {
+                                
+                            } label: {
+                                Image("google-sign-in-icon")
+                                    .resizable()
+                                    .frame(width: 44, height: 44)
+                            }
                         }
                     }
-                }
-                .padding(.vertical)
-                
-                // sign-in button
-                Button {
+                    .padding(.vertical)
                     
-                } label: {
-                    HStack {
-                        Text("SIGN IN")
-                            .foregroundStyle(.black)
+                    // sign-in button
+                    Button {
                         
-                        Image(systemName: "arrow.right")
-                            .foregroundStyle(.black)
+                    } label: {
+                        HStack {
+                            Text("SIGN IN")
+                                .foregroundStyle(.black)
+                            
+                            Image(systemName: "arrow.right")
+                                .foregroundStyle(.black)
+                        }
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 50)
                     }
-                    .frame(width: UIScreen.main.bounds.width - 32, height: 50)
-                }
-                .background(Color(.white))
-                .cornerRadius(10)
-                .padding(.vertical)
+                    .background(Color(.white))
+                    .cornerRadius(10)
+                    .padding(.vertical)
 
-                // signup button
-                Button {
-                    
-                } label: {
-                    HStack {
-                        Text("Don't have an account?")
-                            .font(.system(size: 14))
-                        
-                        Text("Sign Up")
-                            .font(.system(size: 14, weight: .semibold))
+                    // signup button                    
+                    NavigationLink {
+                        RegistrationView()
+                            .navigationBarBackButtonHidden(true)
+                    } label: {
+                        HStack {
+                            Text("Don't have an account?")
+                                .font(.system(size: 14))
+                            
+                            Text("Sign Up")
+                                .font(.system(size: 14, weight: .semibold))
+                        }
+                        .foregroundStyle(.white)
                     }
-                    .foregroundStyle(.white)
                 }
             }
         }
