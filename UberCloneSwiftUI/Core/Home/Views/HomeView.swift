e3a4be267ca8370336cad33a5d2c8e085ca40ccc
DamienBabington <damien.babington@gmail.com>
2024-10-09 13:39:07 +0900

    Add logic to show side menu

--- UberCloneSwiftUI/Core/Home/Views/HomeView.swift
+++ UberCloneSwiftUI/Core/Home/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var mapState = MapViewState.noInput
+    @State private var showSideMenu = false
     @EnvironmentObject var locationViewModel: LocationSearchViewModel
     @EnvironmentObject var authViewModel: AuthViewModel
     
@@ -18,40 +19,54 @@ struct HomeView: View {
             if authViewModel.userSession == nil {
                 LoginView()
             } else {
-                ZStack(alignment: .bottom) {
-                    ZStack(alignment: .top) {
-                        UberMapViewRepresentable(mapState: $mapState)
-                            .ignoresSafeArea()
-                        
-                        if mapState == .searchingForLocation {
-                            LocationSearchView(mapState: $mapState)
-                        } else if mapState == .noInput {
-                            LocationSearchActivationView()
-                                .padding(.top, 72)
-                                .onTapGesture {
-                                    withAnimation(.spring()) {
-                                        mapState = .searchingForLocation
-                                    }
-                                }
+                ZStack {
+                    if showSideMenu {
+                        SideMenuView()
+                    }
+                    mapView
+                        .offset(x: showSideMenu ? 316 : 0)
+                        .shadow(color: showSideMenu ? .black : .clear, radius: 10)
+                }
+            }
+        }
+    }
+}
+
+
+extension HomeView {
+    var mapView: some View {
+        ZStack(alignment: .bottom) {
+            ZStack(alignment: .top) {
+                UberMapViewRepresentable(mapState: $mapState)
+                    .ignoresSafeArea()
+                
+                if mapState == .searchingForLocation {
+                    LocationSearchView(mapState: $mapState)
+                } else if mapState == .noInput {
+                    LocationSearchActivationView()
+                        .padding(.top, 72)
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                mapState = .searchingForLocation
+                            }
                         }
-                        
-                        MapViewActionButton(mapState: $mapState)
-                            .padding(.leading)
-                            .padding(.top, 4)
-                    }
-                    
-                    if mapState == .locationSelected || mapState == .mapUpdated {
-                        RideRequestView()
-                            .transition(.move(edge: .bottom))
-                    }
                 }
-                .edgesIgnoringSafeArea(.bottom)
-                .onReceive(LocationManager.shared.$userLocation) { location in
-                    // this modifier allows us to implement functionality when this observed property is updated
-                    if let location {
-                        locationViewModel.userLocation = location
-                    }
-                }
+                
+                MapViewActionButton(mapState: $mapState, showSideMenu: $showSideMenu)
+                    .padding(.leading)
+                    .padding(.top, 4)
+            }
+            
+            if mapState == .locationSelected || mapState == .mapUpdated {
+                RideRequestView()
+                    .transition(.move(edge: .bottom))
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .onReceive(LocationManager.shared.$userLocation) { location in
+            // this modifier allows us to implement functionality when this observed property is updated
+            if let location {
+                locationViewModel.userLocation = location
             }
         }
     }
@@ -60,4 +75,5 @@ struct HomeView: View {
 #Preview {
     HomeView()
         .environmentObject(LocationSearchViewModel())
+        .environmentObject(AuthViewModel())
 }
