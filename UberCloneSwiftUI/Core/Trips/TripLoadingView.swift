7bf4a2b0e70da461ec4c33cfee678bc6cdc6b6f2
DamienBabington <damien.babington@gmail.com>
2024-10-21 21:04:26 +0900

    Create TripLoadingView

--- UberCloneSwiftUI/Core/Trips/TripLoadingView.swift
+++ UberCloneSwiftUI/Core/Trips/TripLoadingView.swift
@@ -10,9 +10,31 @@ import SwiftUI
 struct TripLoadingView: View {
     var body: some View {
         VStack {
-            Text("Finding you a ride..")
-                .padding()
+            Capsule()
+                .foregroundStyle(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+                .padding(.top, 10)
             
+            HStack {
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Connecting you to a driver")
+                        .font(.headline)
+                    
+                    Text("Arriving at 1:30pm")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(Color(.systemBlue))
+                }
+                .padding()
+                
+                Spacer()
+                
+                Spinner(lineWidth: 6, height: 48, width: 48)
+                    .padding(.vertical)
+                    .padding(.leading)
+                    .padding(.trailing, 36)
+            }
+            .padding(.bottom, 24)
         }
         .background(Color.theme.backgroundColor)
         .cornerRadius(16)
